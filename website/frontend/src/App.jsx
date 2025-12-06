fb57107afe175862753bb56954c2c398b4b2cd07
Amr Mohamed <amr778776@gmail.com>
2025-12-06 08:18:59 +0200

    Fix property detail fetch and navigation

--- website/frontend/src/App.jsx
+++ website/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import AuthPage from './AuthPage'
 import SearchResults from './SearchResults'
+import PropertyDetailsPage from './PropertyDetailsPage'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signin" element={<AuthPage />} />
         <Route path="/signup" element={<AuthPage />} />
         <Route path="/search" element={<SearchResults />} />
+        <Route path="/property/:id" element={<PropertyDetailsPage />} />
       </Routes>
     </Router>
   )
